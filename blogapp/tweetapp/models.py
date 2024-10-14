48e63826881fd24502c66c37ab004c418e25c2eb
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2024-10-14 23:53:16 +0530

    first commit

--- blogapp/tweetapp/models.py
+++ blogapp/tweetapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Tweet(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=240)
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
